cb4f6b28de4e07968dcd62b80feeae6d4fbe7878
Patricia <psd1@rice.edu>
2017-12-03 22:59:48 -0600

    Fully commented and adjusted to read in one specified signal at a time.

--- CTG_Database_Reader.m
+++ CTG_Database_Reader.m
@@ -1,68 +1,98 @@
+%% CTG_Database_Reader 
+% Purpose: 
+%   This code reads a specific file from an online database of uterine
+%   contractions (measured with a toco), performs filtering then uses a
+%   thresholding system to detect contraction peaks
+%
+% Notes: 
+%   rdsamp outputs sig & Fs
+%       sig = N x M matrix where there are M signals (in our case 2, one is FHR
+%       one is UC) w/ ea. signal comprising one column
+%       Fs = 1 x M vector that contains the sampling frequency of the data; in
+%       our case this is 4Hz
+
+%% Setup
+
 clear all; close all; clc;
 
 % Modify location for where you unzipped the wfdp toolkit files, the path
 % needs to be set to the 'mcode' folder
-% addpath('D:\wfdb-app-toolbox-0-10-0\mcode')
-addpath('C:\Users\atolp\Documents\MATLAB\mcode');
+% addpath('D:\wfdb-app-toolbox-0-10-0\mcode') % Dr. Carns' path
+%addpath('C:\Users\atolp\Documents\MATLAB\mcode'); % Aniket's path
+addpath('/Users/Patricia/Documents/MATLAB/mcode'); % Patricia's path
+
+%% Get signal from database
+
 % Read the signal from the website using the rdsamp function. 
-all_thresholds=[];
-entries=[1001:1506 2001:2046];
-for i=1:length(entries)
-    str=strcat('ctu-uhb-ctgdb/',num2str(entries(i)),'.dat');
-[tm,sig]=rdsamp(str); % The rdsamp function has the website 'https://physionet.org/physiobank/database/' embedded within it.  You have to provide the rest of the website information for the database and file you want.
-time=[0:1/4:(length(tm)-1)/4]; % According to the website, the data was sampled at 4 Hz
+[tm,Fs]=rdsamp('ctu-uhb-ctgdb/1001.dat'); % The rdsamp function has the website 'https://physionet.org/physiobank/database/' embedded within it.  You have to provide the rest of the website information for the database and file you want.
+time=0:1/Fs:(length(tm)-1)/Fs; % According to the website, the data was sampled at 4 Hz
 
-% % Plot the traces.  The signal contains both FHR and UC traces.  The first is FHR, the second is UC
-% figure;plot(time,tm(:,1));xlabel('Time (s)');ylabel('FHR (bpm)')
-% figure;plot(time,tm(:,2));xlabel('Time (s)');ylabel('UC (arb. units)') % Since the amplitude of the toco is meaningless, I put the y-axis as 'arbitrary units'.
-% 
-% % Plot a shorter section of the UC trace to allow for better visibility
-% figure;plot(time(1:3000),tm(1:3000,2));xlabel('Time (s)');ylabel('UC (arb. units)') 
+% Plot the traces.  The signal contains both FHR and UC traces.  The first is FHR, the second is UC
+figure;plot(time,tm(:,2));xlabel('Time (s)');ylabel('UC (arb. units)'); title('Raw Signal'); % Since the amplitude of the toco is meaningless, I put the y-axis as 'arbitrary units'.
 
-L=size(tm,1);
-sig=tm(1:L,2);
+% Plot a shorter section of the UC trace to allow for better visibility
+figure;plot(time(1:3000),tm(1:3000,2));xlabel('Time (s)');ylabel('UC (arb. units)'); title('Raw Signal (small)');
 
-fftSig=(fft(sig));
-fltr=fftSig.*0;
+
+%% Filter - Low pass
+
+L=size(tm,1); % length of signal
+sig=tm(1:L,2); % set sig as only the 2nd column, the UC data
+
+fftSig=(fft(sig)); % fourier of signal
+
+fltr=fftSig.*0; % create a masking vector of 1s and 0s
 lowBnd=1;
 highBnd=85;
-fltr(lowBnd:highBnd)=1;
+fltr(lowBnd:highBnd)=1; 
 
 %figure;
-%plot(abs(fftSig));
+%plot(abs(fftSig)); % visualization of the freq. distribution
 % title('FFT of input signal');
 % xlabel('Frequency');
 
-fltSig=fftSig.*fltr;
-%figure;
-%plot(abs(fltSig));
+
+fltSig=fftSig.*fltr; % apply the filter to the signal
+% figure;
+% plot(abs(fltSig));
 % title('Frequency domain filtered input signal');
 % xlabel('Frequency');
 
 
-reconstr=ifft((fltSig));
+reconstr=ifft((fltSig)); % go back to time domain using filtered signal
 phase_rec=angle(reconstr);
 amplitude_rec=abs(reconstr);
 reconstrOut=2*amplitude_rec.*cos(phase_rec);
 x=1:length(sig);
 
-%figure;
-%plot(x,sig,'r',x,reconstrOut,'b');
-% title('FFT filtered, original signals');
-% xlabel('Time');
-% legend('Original','Filtered');
+%% Plot filtered and original signal
 
+figure;
+plot(x,sig,'r',x,reconstrOut,'b'); 
+title('FFT filtered, original signals');
+xlabel('Time');
+legend('Original','Filtered');
 
-% figure;
-% hold on;
+%% Calculate slope of filtered signal
+
+% initialize vector
 slopes=zeros(size(tm));
-thres=zeros(1,L);
-window=10;
+
 for i=1:(L-1)
 slopes(i,2)=50*(reconstrOut(i+1,1)-reconstrOut(i,1))/(time(i+1)-time(i));
+% the 50 is a multiplier used to bring the slope graph to a similar
+% magnitude as the signals
 end
 
-for i=1:L
+
+%% Define threshold
+
+% initialize vectors
+thres=zeros(1,L);
+window=10; % time window in minutes
+
+% calculate threshold via moving average 
+for i=1:L 
     if i<=window*60
         slopeMax=max(slopes(1:i,2));
         slopeMin=min(slopes(1:i,2));
@@ -75,18 +105,25 @@ for i=1:L
         thres(i)=slopeMin+0.63*range;
     end
 end
-
+    
+% calculate threshold via range percentage
 slopeMax=max(slopes);
 slopeMin=min(slopes);
 range=slopeMax(2)-slopeMin(2);
 thres_orig=(slopeMin(2)+0.63*range)*ones(1,length(thres));
-% plot(time(1:L),tm(1:L,2),'r');xlabel('Time (s)');ylabel('UC (arb. units)');
-% plot(time(1:L),slopes(1:L,2),'b');xlabel('Time (s)');ylabel('Signal Slope');
-% plot(time(1:L),reconstrOut,'k');
-% plot(time(1:L),thres,'m');
-% plot(time(1:L),thres_orig,'g');
-% hold off;
-% legend('Raw','Filt. Slopes','Filt. Sig','Thres Moving','Thres Orig');
+
+%% Plot signal (original+filt), slopes, and threshold (2 types)
+
+figure;
+hold on;
+plot(time(1:L),tm(1:L,2),'r');xlabel('Time (s)');ylabel('UC (arb. units)');
+plot(time(1:L),slopes(1:L,2),'b');xlabel('Time (s)');ylabel('Signal Slope');
+plot(time(1:L),reconstrOut,'k');
+plot(time(1:L),thres,'m');
+plot(time(1:L),thres_orig,'g');
+hold off;
+legend('Raw','Filt. Slopes','Filt. Sig','Thres Moving','Thres Orig');
+title('Signal with Overlaid Thresholds')
+
 disp(thres_orig(1));
-all_thresholds=[all_thresholds thres_orig(1)];
-end
+
