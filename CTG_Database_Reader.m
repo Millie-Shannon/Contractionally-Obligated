ca50d81225b294d521dd37049a7c8cabcb91cb14
Patricia <psd1@rice.edu>
2017-12-10 20:17:06 -0600

    This is our finalized code for Cycle 2. It now has the added functionality of loading data from an excel file OR the CTG database. Slope calculation windows and peak counting are fully commented.

--- CTG_Database_Reader.m
+++ CTG_Database_Reader.m
@@ -23,25 +23,43 @@ addpath('/Users/Patricia/Documents/MATLAB/mcode'); % Patricia's path
 
 %% Get signal from database
 
-% define entry to analyze
-entry='1001';
+% % define entry to analyze
+% entry='1312';
+% 
+% % Read the signal from the website using the rdsamp function. 
+% [tm,Fs]=rdsamp(strcat('ctu-uhb-ctgdb/',entry,'.dat')); % The rdsamp function has the website 'https://physionet.org/physiobank/database/' embedded within it.  You have to provide the rest of the website information for the database and file you want.
+% time=0:1/Fs:(length(tm)-1)/Fs; % According to the website, the data was sampled at 4 Hz
+% time = time./60; % convert from seconds to minutes
+% 
+% % Plot the raw signal.  The signal contains both FHR and UC traces.  The first is FHR, the second is UC
+% figure;plot(time,tm(:,2));xlabel('Time (min)');ylabel('UC (arb. units)'); title('Raw Signal'); % Since the amplitude of the toco is meaningless, I put the y-axis as 'arbitrary units'.
+% 
+% % Plot a shorter section of the UC trace to allow for better visibility
+% figure;plot(time(1:3000),tm(1:3000,2));xlabel('Time (min)');ylabel('UC (arb. units)'); title('Raw Signal (small)');
+% 
+% sig=tm(1:L,2); % set sig as only the 2nd column, the UC data
 
-% Read the signal from the website using the rdsamp function. 
-[tm,Fs]=rdsamp(strcat('ctu-uhb-ctgdb/',entry,'.dat')); % The rdsamp function has the website 'https://physionet.org/physiobank/database/' embedded within it.  You have to provide the rest of the website information for the database and file you want.
-time=0:1/Fs:(length(tm)-1)/Fs; % According to the website, the data was sampled at 4 Hz
-time = time./60; % convert from seconds to minutes
+%% Get signal from excel - Optoco Data
 
-% Plot the traces.  The signal contains both FHR and UC traces.  The first is FHR, the second is UC
-figure;plot(time,tm(:,2));xlabel('Time (min)');ylabel('UC (arb. units)'); title('Raw Signal'); % Since the amplitude of the toco is meaningless, I put the y-axis as 'arbitrary units'.
+% select folder where excel file is located
+[fname, pname] = uigetfile('*.*','MultiSelect', 'on','Choose spreadsheet for analysis');
 
-% Plot a shorter section of the UC trace to allow for better visibility
-figure;plot(time(1:3000),tm(1:3000,2));xlabel('Time (min)');ylabel('UC (arb. units)'); title('Raw Signal (small)');
+% read in both columns 
+time = xlsread(strcat(pname,fname), 'A:A');
+time = time/60; % convert to minutes
+sig = xlsread(strcat(pname,fname), 'B:B');
 
+% write in Optoco's sampling frequency
+Fs = 1000;
+
+% display raw signal
+figure;
+hold on;
+plot(time,sig);xlabel('Time (min)');ylabel('UC (arb. units)'); title('Optoco Raw Signal');
 
 %% Filter - Low pass
 
-L=size(tm,1); % length of signal
-sig=tm(1:L,2); % set sig as only the 2nd column, the UC data
+L=size(sig,1); % length of signal
 
 fftSig=(fft(sig)); % fourier of signal
 
@@ -53,7 +71,7 @@ P1(2:end-1) = 2*P1(2:end-1);
 
 figure;
 plot(f,P1);
-title('Fourier of Signal - Online Method')
+title('Fourier of Signal')
 
 
 % Method 2 - pat and aniket
@@ -65,7 +83,7 @@ title('Fourier of Signal - Same Length')
 fltr=fftSig.*0; % create a masking vector of 1s and 0s
 
 lowF=0.0005; % low cutoff frequency (Hz);
-highF = 0.025; % high cutoff frequency (Hz); 
+highF = 0.15; % high cutoff frequency (Hz); 
 
 % Pat tried finding the index of the cutoff frequencies in the f2 vector
 % index_lo = find(f2 == lowF);
@@ -73,8 +91,8 @@ highF = 0.025; % high cutoff frequency (Hz);
 % fltr(index_lo:index_hi) = 1;
 
 % Aniket mapped frequency to sample index using f = Fs * (0:(L/2))/L or f = Fs*(0:L-1)/(2*L);
-array_cut_low=floor(1+(lowF/(Fs))*2*L)
-array_cut_high=ceil(1+(highF/(Fs))*2*L)
+array_cut_low=floor(1+(lowF/(Fs))*2*L);
+array_cut_high=ceil(1+(highF/(Fs))*2*L);
 fltr(array_cut_low:array_cut_high)=1;
 
 
@@ -102,12 +120,14 @@ legend('Original','Filtered');
 %% Calculate slope of filtered signal
 
 % initialize vector
-slopes=zeros(size(tm));
+slopes=zeros(size(sig));
+slope_window= Fs*(60 * 0.07); % this is an X minute window (change last number)
 
-for i=1:(L-1)
-slopes(i,2)=1*(reconstrOut(i+1,1)-reconstrOut(i,1))/(time(i+1)-time(i));
-% the 50 is a multiplier used to bring the slope graph to a similar
-% magnitude as the signals
+for i=(slope_window+1):(L)
+    
+    slopes(i,2)=1*(reconstrOut(i,1)-reconstrOut(i-slope_window,1))/(time(i)-time(i-slope_window));
+    % the first # is a multiplier used to bring the slope graph to a similar
+    % magnitude as the signals
 end
 
 
@@ -116,6 +136,7 @@ end
 % initialize vectors
 thres=zeros(1,L);
 window=10; % time window in minutes
+ratio = 0.67;
 
 % time=0:1/Fs:(length(tm)-1)/Fs *** REMINDER! ea. sample is spaced as such
 
@@ -125,12 +146,12 @@ for i=1:L
         slopeMax=max(slopes(1:i,2));
         slopeMin=min(slopes(1:i,2));
         range=slopeMax-slopeMin;
-        thres(i)=slopeMin+0.63*range;
+        thres(i)=slopeMin+ratio*range;
     else
         slopeMax=max(slopes((i-window*60*Fs):i,2));
         slopeMin=min(slopes((i-window*60*Fs):i,2));
         range=slopeMax-slopeMin;
-        thres(i)=slopeMin+0.63*range;
+        thres(i)=slopeMin+ratio*range;
     end
 end
     
@@ -138,13 +159,13 @@ end
 slopeMax=max(slopes);
 slopeMin=min(slopes);
 range=slopeMax(2)-slopeMin(2);
-thres_orig=(slopeMin(2)+0.63*range)*ones(1,length(thres));
+thres_orig=(slopeMin(2)+ratio*range)*ones(1,length(thres));
 
 %% Plot signal (original+filt), slopes, and threshold (2 types)
 
 figure;
 hold on;
-plot(time(1:L),tm(1:L,2),'r');xlabel('Time (min)');ylabel('UC (arb. units)');
+plot(time(1:L),sig,'r');xlabel('Time (min)');ylabel('UC (arb. units)');
 plot(time(1:L),reconstrOut,'k');
 plot(time(1:L),slopes(1:L,2),'b');xlabel('Time (min)');ylabel('Signal Slope');
 plot(time(1:L),thres,'m');
@@ -154,9 +175,10 @@ legend('Raw','Filt. Sig','Filt. Slopes','Thres Moving','Thres Orig');
 title('Signal with Overlaid Thresholds');
 
 %% Peak Counting
-count = [];
-ind = 1;
-k = 1;
+count = []; % this is a #contractions x 2 matrix with the index of the slopes vector on the up and down crossing of the threshold
+ind = 1; % this index keeps track of the row in the count matrix
+k = 1; % this k allows us to step through each point in slopes vector
+t = 0.1; % this is the acceptabe time spacing between peaks in minutes 
 
 while k < length(slopes)
     % if you are above the threshold...
@@ -170,14 +192,14 @@ while k < length(slopes)
         % once you're below threshold, check to make sure peak isn't too
         % close to previous one...
         if ind > 1
-            hi = k+add
-            hi2 = count(ind-1,1)
-            time_dif = (k+add-count(ind-1,1))/Fs/60
+            time_dif = (k+add-count(ind-1,1))/Fs/60; 
+            % index of detected peak - index of previous counted peak
+            % converted to seconds (/Fs) then minutes (/60)
         else
-            time_dif = 2;
+            time_dif = 2; % if this is the first detected peak, allow it to count! 
         end
         
-        if time_dif > 1 % only if the peak is further than 1 min apart
+        if time_dif > t % only if the peak is further than t min apart
             count(ind,2) = k + add; % save that 2nd crossover point
             ind = ind + 1; % advance the index of your count matrix
         end
@@ -191,7 +213,7 @@ while k < length(slopes)
     end
     
 end
-count
+
 tot_contr = length(count);
 disp('Our contraction count:');
 disp(tot_contr);
@@ -200,6 +222,31 @@ disp(tot_contr);
 y_contr = ones(1,tot_contr).*thres_orig(1);
 plot(time(count(:,1)),y_contr,'mx','LineWidth',12);
 hold off;
+
+% Use the following to produce figures for presentations/papers
+% fig=figure;
+% hold on;
+% a=plot(time(1:L),tm(1:L,2),'r');
+% b=plot(time(1:L),reconstrOut,'b');
+% c=plot(time(1:L),slopes(1:L,2),'LineWidth',2);
+% c.Color=[39 139 34]./255;
+% d=plot(time(1:L),thres_orig,'LineWidth',3.5);
+% d.Color=[0 0 0]./255;
+% 
+% 
+% % plot the found points
+% y_contr = ones(1,tot_contr).*thres_orig(1);
+% e=plot(time(count(:,1)),y_contr,'x','LineWidth',18);
+% e.Color=[255 140 0]./255;
+% 
+% legend('Raw','Filt. Sig','Filt. Sig Slopes','Threshold',...
+%     'Detected Contractions','Location','southeast');
+% % set(gca,'FontSize',30)
+% xlabel('Time (min)','FontSize',30);
+% ylabel('UC (arb. units)','FontSize',30);
+% title('Uterine Contraction Original and Procesed Signals with Overlaid Thresholds','FontSize',25);
+% axis([0 25 -70 110]);
+% hold off;
 
 
 %% Output to Excel
